99e8d8bf01c88ab2ecd16e1bda5e73811fcfcb3b
anvesharma <anvesh16@gmail.com>
2026-06-07 14:39:39 -0700

    Fix API URL env var

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api',
 });
 
-// Attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
-// Handle 401 globally
 api.interceptors.response.use(
   (res) => res,
   (err) => {
