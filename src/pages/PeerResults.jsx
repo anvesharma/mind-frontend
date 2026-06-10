a51e7517c14bdaf6dde91c11e7cb3c03bf782d6a
anvesharma <anvesh16@gmail.com>
2026-06-10 16:59:40 -0700

    Use plain axios in PeerResults to avoid auth interceptor

--- src/pages/PeerResults.jsx
+++ src/pages/PeerResults.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import api from '../api';
+import axios from 'axios';
 import './PersonalResults.css';
 
 const TYPE_DATA = {
