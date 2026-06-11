8f7f1d4301a98eb45f6689074ad7c97fa4fedefd
anvesharma <anvesh16@gmail.com>
2026-06-11 16:21:47 -0700

    Fix QRCode import

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import QRCode from 'qrcode.react';
+import { QRCode } from 'qrcode.react';
 import './Landing.css';
 
 function NebulaOrb({ size, colors }) {
